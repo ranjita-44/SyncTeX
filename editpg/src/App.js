903b8514b0e7ad24747996c3878e5bb393ea34cc
ashwini <ashwinii.subedi@gmail.com>
2024-06-06 23:36:28 +0545

    routes configuration

--- editpg/src/App.js
+++ editpg/src/App.js
@@ -1,12 +1,16 @@
-import React from 'react'
+import React from "react";
+import { Routes} from "react-router-dom";
+import { Home } from "./container";
 
 
-const app = () => {
+const App = () => {
   return (
-    <div className= "w-screen h-screen flex items-center justify-center">
-      app
+    <div className="w-screen h-screen flex items-start justify-start overflow-hidden">
+      <Routes>
+        <route path="/home/*" element={<Home/>} />
+      </Routes>
     </div>
-  )
-} 
+  );
+};
 
-export default app
+export default App;
