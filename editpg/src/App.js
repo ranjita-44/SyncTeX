76fe1e8a477dd5a87002461f8aff9adb625bedb5
notashwinii <ashwinii.subedi@gmail.com>
2024-06-08 14:09:44 +0545

    sidebar

--- editpg/src/App.js
+++ editpg/src/App.js
@@ -1,16 +1,20 @@
 import React from "react";
-import { Routes} from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import { Home } from "./container";
 
-
 const App = () => {
-  return (
-    <div className="w-screen h-screen flex items-start justify-start overflow-hidden">
-      <Routes>
-        <route path="/home/*" element={<Home/>} />
-      </Routes>
-    </div>
-  );
+    return (
+        <div className="w-screen h-screen flex items-start justify-start overflow-hidden">
+
+            <Routes>
+
+                <Route path="/home/*" element={<Home />} />
+
+                {/*if the route not match*/}
+                <Route path="*" element={<Navigate to={"/home"}/>}/>
+            </Routes>
+        </div>
+    );
 };
 
 export default App;
--- editpg/src/container/Home.jsx
+++ editpg/src/container/Home.jsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react'
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
+    const[isSideMenu, setIsSideMenu] = useState(false)
+  return <>
+  
+  <div className={`w-2 ${isSideMenu ? "w-2" : "flex-[.2] xl:flex-[.2]"} min-h-screen max-h-screen relative bg-secondary`}>
+
+    {/* anchor*/}
+    <div className="w-8 h-8 bg-white rounded-tr-lg rounded-br-lg absolute -right-6 flex items-center justify-center cursor-pointer"></div>
+
+  </div>
+  <div></div>
+  </>
 }
 
-export default Home;
+export default Home
