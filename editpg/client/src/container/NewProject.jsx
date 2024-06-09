c519da81d3a444fac6836682ba6d60569fd3f12e
notashwinii <ashwinii.subedi@gmail.com>
2024-06-09 08:42:37 +0545

    added framer

--- editpg/client/src/container/NewProject.jsx
+++ editpg/client/src/container/NewProject.jsx
@@ -4,11 +4,19 @@ import Splitpane from "react-split-pane";
 import CodeMirror from '@uiw/react-codemirror';
 import { javascript } from '@codemirror/lang-javascript';
 
+import { motion, AnimatePresence } from "framer-motion";
+
 import synctex from '../assets/synctex.svg';
 
 
+
 const NewProject = () => {
+
+  const [isTitle, setisTitle] = useState("");
+
   return <>
+
+
 
     <div className="w-screen h-screen flex flex-col items-start justify-start overflow-hidden">
 
@@ -21,15 +29,34 @@ const NewProject = () => {
           {/*<Link to={"/home/projects"}> */}
 
           <img className="px-2
-          w-12 h-auto object-contain" src={synctex} alt="SyncTeX logo"/>
+          w-12 h-auto object-contain" src={synctex} alt="SyncTeX logo" />
+
+          {/*</Link> */}
+
+
+          <div className="flex flex-col items-start justify-start">
+            {/*title */}
+
+            <div className="flex items-center justify-center gap3">
+              <AnimatePresence> {isTitle ? <>
+
+              </> : <> </>}
+
+              </AnimatePresence>
+
+
+            </div>
+          </div>
+
+
+
         </div>
 
       </header>
-      
-     
 
       {/*Main Section */}
       <div className="w-full h-full flex flex-grow overflow-hidden">
+
         {/*coding section */}
 
 
@@ -37,37 +64,57 @@ const NewProject = () => {
 
           {/*file Navigation*/}
           <div className="flex flex-col">
+
             <div className="bg-[#5f6063] p-4 h-12 flex items-center justify-between">
+
               <h2 className="text-lg font-semibold">Pane 1</h2>
+
             </div>
+
             <div className="flex-grow bg-[#5f6063] "  >
               {/* Content for Pane 1 */}
+
             </div>
+
           </div>
-         
+
           {/*code editor */}
           <Splitpane split="vertical" minSize={100} maxSize={-100} defaultSize={"50%"}>
 
-          <div className="flex flex-col">
-            <div className="bg-[#5f6063] p-1 h-12 flex items-center justify-between">
-              <h2 className="text-lg font-semibold">Code Editor</h2>
-            </div>
-            <div className="flex-grow">
+            <div className="flex flex-col">
+              <div className="bg-[#5f6063] p-1 h-12 flex items-center justify-between">
 
-              <CodeMirror value="console.log('hello');" height="600rem" extensions={[javascript({ jsx: true })]} onChange={() => { }} />;
-            </div>
-          </div>
-          {/*preview */}
-          <div className="flex flex-col">
-            <div className="bg-[#5f6063] p-1 h-12 flex items-center justify-between">
-              <h2 className="text-lg font-semibold">Preview</h2>
-              <button className="bg-[#2cb7b0] text-white px-4 py-1 rounded">Compile</button>
-            </div>
-            <div className="flex-grow">
-            </div>
-          </div>
+                <h2 className="text-lg font-semibold">Code Editor</h2>
 
-</Splitpane>
+              </div>
+
+              <div className="flex-grow">
+
+                <CodeMirror value="console.log('hello');" height="600rem" extensions={[javascript({ jsx: true })]} onChange={() => { }} />;
+
+              </div>
+
+            </div>
+
+            {/*preview */}
+
+            <div className="flex flex-col">
+
+              <div className="bg-[#5f6063] p-1 h-12 flex items-center justify-between">
+
+                <h2 className="text-lg font-semibold">Preview</h2>
+
+                <button className="bg-[#2cb7b0] text-white px-4 py-1 rounded">Compile</button>
+
+              </div>
+
+              <div className="flex-grow">
+
+              </div>
+
+            </div>
+
+          </Splitpane>
         </Splitpane>
 
       </div>
