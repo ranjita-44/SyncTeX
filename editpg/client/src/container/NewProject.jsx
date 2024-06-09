98d1409b1dde5bd87f4abd2244aad01e9553718c
notashwinii <ashwinii.subedi@gmail.com>
2024-06-09 06:48:01 +0545

    compile btn+header

--- editpg/client/src/container/NewProject.jsx
+++ editpg/client/src/container/NewProject.jsx
@@ -12,29 +12,47 @@ const NewProject = () => {
       {/*alert section */}
 
       {/*header section */}
+      <div className="w-full flex items-start justify-between bg-gray-200 p-1">
+        <h1 className="text-xl font-bold">Header</h1>
+        <button className="bg-blue-500 text-white px-4 py-2 rounded">Button</button>
+      </div>
 
-      {/*coding section */}
-      <div id="code_section">
+      <div>
+        {/*coding section */}
+
 
         <Splitpane split="vertical" minSize={100} max={-100} defaultSize={"50%"}>
 
-          <div className="w-full px-2">
-          <CodeMirror value="console.log('hello');" height="600rem" extensions={[javascript({ jsx: true })]} onChange={() =>{}} />;
+          <div className="w-full h-full px-2 flex flex-col">
+            <div className="bg-gray-300 p-1">
+              <h2 className="text-lg font-semibold">Code Editor </h2>
+
+            </div>
+            <div className="flex-grow">
+
+              <CodeMirror value="console.log('hello');" height="600rem" extensions={[javascript({ jsx: true })]} onChange={() => { }} />;
+            </div>
+          </div>
+
+          <div className="w-full h-full flex flex-col">
+            <div className="bg-gray-300 p-1 flex items-center justify-between">
+              <h2 className="text-lg font-semibold">Preview</h2>
+              <button className="bg-blue-500 text-white px-4 py-1 rounded">Compile</button>
+            </div>
+            <div className="flex-grow">
+           </div>
           </div>
 
 
-
-          <div className="w-full h-full flex items-start justify-start"></div>
-
-
         </Splitpane>
+
       </div>
-
-
-
-
-
     </div>
+
+
+
+
+
 
   </>
 }
